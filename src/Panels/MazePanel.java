d5174d9575dd9aef13eadb006609de5d30b97be6
jszym99 <jszymczyk99@gmail.com>
2023-06-13 17:18:56 +0200

    Add collisions

--- src/Panels/MazePanel.java
+++ src/Panels/MazePanel.java
@@ -2,6 +2,8 @@ package Panels;
 
 import SceneObjects.MazeGenerator;
 import SceneObjects.PacmanObject;
+import SceneObjects.SceneObject;
+import SceneObjects.Wall;
 
 import javax.swing.*;
 import java.awt.*;
@@ -9,7 +11,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.beans.PropertyChangeListener;
 
 public class MazePanel extends JPanel implements ActionListener {
     private final MazeGenerator mazeGenerator;
@@ -57,6 +58,53 @@ public class MazePanel extends JPanel implements ActionListener {
         addKeyListener(pacmanKeyAdapter);
     }
 
+    public void checkCollisions()
+    {
+        int width = 28;
+        int pacmanPosX = pacman.getInfo().get("X")/pacman.getInfo().get("Width");
+        int pacmanPosY = pacman.getInfo().get("Y")/pacman.getInfo().get("Height");
+        int pacmanDirection = pacman.getCurrentDirection();
+        int xOffset = 0;
+        int yOffset = 0;
+        SceneObject block;
+
+        switch (pacmanDirection){
+            case 0:
+                xOffset = -1;
+                yOffset = 0;
+                break;
+            case 1:
+                xOffset = 0;
+                yOffset = -1;
+                break;
+            case 2:
+                xOffset = 1;
+                yOffset = 0;
+                break;
+            case 3:
+                xOffset = 0;
+                yOffset = 1;
+                break;
+            default:
+                break;
+        }
+
+        block = mazeGenerator.getWalls().get((pacmanPosY+yOffset-3)*width+(pacmanPosX+xOffset));
+//        System.out.print("X:");
+//        System.out.print(pacmanPosX+xOffset);
+//        System.out.print("Y:");
+//        System.out.println(pacmanPosY+yOffset);
+        if(block instanceof Wall) {
+            System.out.println("Wall!!!");
+            if (pacman.getRect().intersects(block.getRect())) {
+//                System.out.println("Intersects");
+                pacman.setCollision(true);
+            }
+        }
+        else
+            pacman.setCollision(false);
+    }
+
     @Override
     public void paintComponent(Graphics graphics)
     {
@@ -68,6 +116,7 @@ public class MazePanel extends JPanel implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         Thread thread = new Thread(pacman);
         thread.start();
+        checkCollisions();
         repaint();
     }
 }
--- src/SceneObjects/PacmanObject.java
+++ src/SceneObjects/PacmanObject.java
@@ -7,7 +7,9 @@ import java.awt.*;
 
 public class PacmanObject extends SceneObject implements Runnable {
     private final Controls controls = new Controls();
+    private int currentDirection;
     private int lastDirection;
+    private boolean collision;
 
     private Image[] pacmanImages = new Image[4];
     public PacmanObject(int new_x, int new_y) {
@@ -21,42 +23,61 @@ public class PacmanObject extends SceneObject implements Runnable {
         height = image.getHeight(null);
         x = new_x * width;
         y = new_y * height;
-        rectangle = new Rectangle(x-1,y-1,width+2,height+2);
+//        rectangle = new Rectangle(x-1,y-1,width+2,height+2);
+        setRect();
 
     }
 
     @Override
     public void run() {
-        int currentDirection = controls.getDirection();
-        if(currentDirection != lastDirection)
-            lastDirection = currentDirection;
-            switchImage(currentDirection);
+        int newDirection = controls.getDirection();
+
         switch (currentDirection){
             case 0:
-                x = x-4;
+                if(!collision)
+                    x = x-4;
+                setRect();
 //                System.out.println("Left");
                 break;
             case 1:
 //                System.out.println("Up");
-                y = y-4;
+                if(!collision)
+                    y = y-4;
+                setRect();
                 break;
             case 2:
 //                System.out.println("Right");
-                x = x+4;
+                if(!collision)
+                    x = x+4;
+                setRect();
                 break;
             case 3:
 //                System.out.println("Down");
-                y = y+4;
+                if(!collision)
+                    y = y+4;
+                setRect();
                 break;
             default:
                 break;
         }
+        if(newDirection != currentDirection && x%width == 0 && y%height == 0)
+            currentDirection = newDirection;
+        switchImage(currentDirection);
     }
 
     private void switchImage(int direction) {
         image = pacmanImages[direction];
     }
+    private void setRect() {
+        rectangle = new Rectangle(x-1,y-1,width+2,height+2);
+    }
 
+    public int getCurrentDirection()
+    {
+        return currentDirection;
+    }
 
     public Controls getKeyAdapter(){return controls;}
+
+    public void setCollision(boolean coll) {collision = coll;}
 }
