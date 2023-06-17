395500867ef0d437f5605f0a6c8e6a2ffa1984e7
AntekJeziorski <antonijeziorski@gmail.com>
2023-06-17 17:41:10 +0200

    add ghosts constructors

--- src/SceneObjects/Blinky.java
+++ src/SceneObjects/Blinky.java
@@ -1,8 +1,19 @@
 package SceneObjects;
 
+import javax.swing.*;
+
 public class Blinky extends Ghost{
     public Blinky(int new_x, int new_y) {
-        super(new_x, new_y);
+        ghostImages[0] = new ImageIcon("src/images/ghost_test.png").getImage();
+        ghostImages[1] = new ImageIcon("src/images/ghost_test.png").getImage();
+        ghostImages[2] = new ImageIcon("src/images/ghost_test.png").getImage();
+        ghostImages[3] = new ImageIcon("src/images/ghost_test.png").getImage();
+        image = new ImageIcon("src/images/ghost_test.png").getImage();
+        width = image.getWidth(null);
+        height = image.getHeight(null);
+        x = new_x * width;
+        y = new_y * height;
+        setRect();
     }
 
     public void getPacmanPos(PacmanObject pacman) {
--- src/SceneObjects/Ghost.java
+++ src/SceneObjects/Ghost.java
@@ -9,13 +9,11 @@ public class Ghost extends SceneObject implements Runnable {
     protected int pacmanY;
     protected int direction;
 
-    public Ghost(int new_x, int new_y) {
-        image = new ImageIcon("src/images/ghost_test.png").getImage();
-        width = image.getWidth(null);
-        height = image.getHeight(null);
-        x = new_x * width;
-        y = new_y * height;
-        setRect();
+    protected boolean [] collision;
+
+    protected Image[] ghostImages = new Image[4];
+
+    public Ghost() {
     }
 
 
@@ -27,7 +25,7 @@ public class Ghost extends SceneObject implements Runnable {
 
     public void getPacmanPos(PacmanObject pacman){}
 
-    private void setRect() {
+    protected void setRect() {
         rectangle = new Rectangle(x-1,y-1,width+2,height+2);
     }
 
